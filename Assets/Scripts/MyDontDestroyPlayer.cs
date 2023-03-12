d53b7c848b3a72264437df2b25f2b9e0367c250e
doramail <doramail@users.noreply.github.com>
2023-03-12 22:05:42 +0100

    MainPlayer Copied into each Scene Still WIP

---
+++ Assets/Scripts/MyDontDestroyPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDontDestroyPlayer : MonoBehaviour
+{
+    public static MyDontDestroyPlayer instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        GameObject.DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
